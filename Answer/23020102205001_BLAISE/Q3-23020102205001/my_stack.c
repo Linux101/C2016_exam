fd3f36df679afeb3124c3e5f9528ca2ddbabf1ec
23020102205001Blaise_Armand <23020102205001@stu.xmu.edu.cn>
2016-05-30 22:01:11 +0800

    answer three

--- Answer/23020102205001_BLAISE/Q3-23020102205001/my_stack.c
+++ Answer/23020102205001_BLAISE/Q3-23020102205001/my_stack.c
@@ -4,5 +4,50 @@
  * 但是记住g++才是编译C++的编译器
  *
  */
+#include "my_stack.h"
+
+int stack_is_full(Stack *pS)
+{
+   
+   if(pS->top == pS->size-1)
+     /* stack is full */
+     return(1);
+   else
+     return(0);
+}
 
 
+
+int stack_is_empty(Stack *pS)
+{
+  
+   if(pS->top == -1)
+     /* stack is empty */
+     return(1);
+   else
+     return(0);
+}
+
+
+void stack_push (int stack[], int x) {
+	if (top == (MAX-1))
+		status = 0; else {
+		status = 1;
+		++top;
+		stack [top] = x;
+	}
+}
+
+
+int stack_pop (int stack[]) {
+	int ret;
+	if (top == -1) {
+		ret = 0;
+		status = 0;
+	} else {
+		status = 1;
+		ret = stack [top];
+		--top;
+	}
+	return ret;
+}
